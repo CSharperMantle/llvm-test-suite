480eef97ad0ff61e342a2853a7b40d4320b42948
Rong Bao <rong.bao@csmantle.top>
2026-05-22 12:52:25 +0800

    Refine test scripts

--- test.sh
+++ test.sh
@@ -3,19 +3,15 @@
 set -u
 
 LLVM_PATH="$1"
-
 if [[ -z "$LLVM_PATH" ]]; then
-	echo "Usage: $0 <llvm_path>"
+	echo "Usage: $0 <llvm_path>" >&2
 	exit 1
 fi
-
 BUILD_DIR=build
-
-mkdir -p "$BUILD_DIR" || true
-cd "$BUILD_DIR" || exit 2
 
 cmake \
 	-G Ninja \
+	-B "$BUILD_DIR" \
 	-DCMAKE_C_COMPILER="$LLVM_PATH"/bin/clang \
 	-DCMAKE_CXX_COMPILER="$LLVM_PATH"/bin/clang++ \
 	-DCMAKE_LINKER_TYPE=LLD \
@@ -23,41 +19,58 @@ cmake \
 	-DCMAKE_CXX_FLAGS='-Wl,-q' \
 	-DCMAKE_JOB_POOLS='link_pool=6' \
 	-DCMAKE_JOB_POOL_LINK='link_pool' \
-	-C../cmake/caches/O3.cmake \
-	../ \
+	-C cmake/caches/O3.cmake \
+	. \
 	|| exit 3
+ninja -C "$BUILD_DIR" || exit 3
 
-ninja || exit 3
-"$LLVM_PATH"/bin/llvm-lit -v -j "$(nproc)" -o ../results-s1.json .
+"$LLVM_PATH"/bin/llvm-lit -sv -o results-s1.json "$BUILD_DIR" || {
+	echo 'XXX Error: baseline tests failed; see results-s1.json' >&2
+	exit 3
+}
 
-cd .. || exit 2
 truncate --size=0 e.log
 
 i=0
-for f in $(find "$BUILD_DIR" -type f -executable -not -name "*.orig" -not -name "*.stripped" -not -path "$BUILD_DIR/tools/*"); do
-	if file "$f" | grep 'ELF' > /dev/null ; then
+while IFS='' read -r -d '' f
+do
+	if file "$f" | grep -F 'ELF' >/dev/null ; then
 		((i++))
-		if true; then
-			if [ ! -e "$f".bolt ]; then
-				echo -e "\n\nXXX [$i] BOLT: ${f}\n"
-				if [ ! -e "$f".orig ]; then
-					cp "$f"{,.orig} -v
-				fi
-				echo -e "\n\n"
-				if ! "$LLVM_PATH"/bin/llvm-bolt "$f".orig -o "$f" --no-threads -reorder-functions=hfsort -split-functions -split-all-cold; then
-					echo -e "\n\nXXX Error: $f\n"
-					echo "Error: $f" >> e.log
-				else
-					touch "$f".bolt
-				fi
+		if [ ! -e "$f".bolt-converted ]; then
+			printf 'XXX [%d] BOLT: %s\n' "$i" "$f" >&2
+			if [ ! -e "$f".orig ]; then
+				cp -v "$f" "$f".orig
 			fi
-		else
-			rm -f "$f" -v
-			rm -f "$f".orig -v
-			rm -f "$f".bolt -v
+			if stdout=$("$LLVM_PATH"/bin/llvm-bolt \
+				"$f".orig \
+				-o "$f" \
+				-reorder-functions=hfsort \
+				-split-functions \
+				-split-all-cold 2>&1) ;
+			then
+				touch "$f".bolt-converted
+			else
+				printf 'XXX Error: %s\n%s\n\n' "$f" "$stdout" >&2
+				printf -- '--- Error: %s ---\n%s\n\n' "$f" "$stdout" >> e.log
+			fi
 		fi
 	fi
-done
+done < <(find "$BUILD_DIR" -type f -executable -not -name '*.orig' -not -name '*.stripped' -not -path "$BUILD_DIR/tools/*" -print0)
 
-cd "$BUILD_DIR" || exit 2
-"$LLVM_PATH"/bin/llvm-lit -v -j "$(nproc)" -o ../results-s2.json .
+"$LLVM_PATH"/bin/llvm-lit -sv -o results-s2.json "$BUILD_DIR"
+
+echo 'XXX Restoring BOLTed files...' >&2
+while IFS='' read -r -d '' f
+do
+	if file "$f" | grep -F 'ELF' >/dev/null ; then
+		((i++))
+		if [ -e "$f".bolt-converted ]; then
+			if [ -e "$f".orig ]; then
+				rm -f "$f"
+				mv "$f".orig "$f"
+			else
+				printf 'XXX Error: cannot find backup file: %s\n' "$f".orig >&2
+			fi
+		fi
+	fi
+done < <(find "$BUILD_DIR" -type f -executable -not -name '*.orig' -not -name '*.stripped' -not -path "$BUILD_DIR/tools/*" -print0)
